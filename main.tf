7871d0f1cc90c167851f1d78af8077704b5eed19
Jonathan <farinloyejonathan@gmail.com>
2021-10-15 17:58:55 +0100

    add user_data

--- main.tf
+++ main.tf
@@ -133,6 +133,16 @@ resource "aws_instance" "app-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.watch3rr-jade.key_name
 
+    user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                EOF
+    
+    # user_data = file("path-to-script.sh")
+
     tags = {
       "Name" = "${var.env_prefix}-server"
     }
